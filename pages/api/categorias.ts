f84140324ea983a39af66944a28a74d4088123ed
Ernest <dite_malacoda@hotmail.com>
2023-02-08 08:11:39 -0600

    integración Prisma en /api

---
+++ pages/api/categorias.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+import { ICategoria } from '@/interfaces';
+
+const prisma = new PrismaClient();
+
+type Data = { name: string } | ICategoria[];
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+	const categorias = await prisma.categoria.findMany();
+
+	res.status(200).json(categorias);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
-import { NextPage } from 'next';
+/**
+ * Prisma solo corre en el Servidor (backend)
+ */
 
-const HomePage: NextPage = () => {
+import { GetServerSideProps, NextPage } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+import { ICategoria } from '../interfaces/categorias';
+
+interface Props {
+	categorias: ICategoria[];
+}
+
+const HomePage: NextPage<Props> = ({ categorias }) => {
 	return <div className='bg-red-500'>HomePage</div>;
+};
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+	const prisma = new PrismaClient();
+
+	const categorias = await prisma.categoria.findMany();
+
+	return {
+		props: {
+			categorias
+		}
+	};
 };
 
 export default HomePage;
